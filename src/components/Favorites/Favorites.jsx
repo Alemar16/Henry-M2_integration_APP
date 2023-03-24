ecee01812473d50a9326a6125fb1939ca44cd40c
Armando Martinez <alemar.martinez16@gmail.com>
2023-03-24 15:14:20 -0300

    Agregar opcion "restablecer" en section filter

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -30,35 +30,54 @@ const Favorites = () => {
   return (
     <div className={styles.background}>
       <h1>Mis Favoritos</h1>
-      
+
       <div className={styles.selectores}>
         <select id="orderSelect" onChange={handleOrder}>
-          <option value="order" disabled>Ordenar en</option>
+          <option value="order" disabled>
+            Ordenar en
+          </option>
           <option value="Ascendente">Ascendente</option>
           <option value="Decendente">Descendente</option>
         </select>
-        <select id="filterSelect" onChange={handleFilter}>
-          <option value="filter" disabled>Filtrar por</option>
+
+        <select
+          id="filterSelect"
+          onChange={(event) => {
+            if (event.target.value === "reset") {
+              handleReset();
+            } else {
+              handleFilter(event);
+            }
+          }}
+        >
+          <option value="filter" disabled>
+            Filtrar por
+          </option>
           <option value="Male">Masculino</option>
           <option value="Female">Femenino</option>
           <option value="Genderless">Sin Genero</option>
           <option value="unknown">Desconocido</option>
+          <option value="reset">Restablecer</option>
         </select>
-        <button value="reset" onClick={handleReset}>Restablecer</button>
-      </div>
 
+    {/*  <button value="reset" onClick={handleReset}>
+          Restablecer
+        </button> */}
+      </div>
 
       <div className={styles.container}>
         {favorites.map((fav) => (
           <Card key={fav.id} {...fav} isFavorite={true} />
         ))}
       </div>
-
     </div>
   );
 }
 
 export default Favorites;
 
-
+/* NOTA (opcion reset)
+  El evento onChange llama a una función anónima que comprueba si la opción
+  seleccionada es "reset". Si es así, se llama a la función handleReset. De lo
+  contrario, se llama a la función handleFilter. */
 
