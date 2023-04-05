71528727dce9563938da4048bf9989b8bc1f88a2
Armando Martinez <alemar.martinez16@gmail.com>
2023-04-04 21:20:52 -0300

    Implementacion del Back-end

--- src/components/Redux/action.types.js
+++
@@ -1,4 +0,0 @@
-export const ADD_FAVORITE = "ADD_FAVORITE";
-export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
-export const FILTER = "FILTER";
-export const ORDER = "ORDER";
--- src/components/Titulo/Titulo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import style from "../Titulo/Titulo.module.css"
-/* import font from "../../assets/Font/Rick-and-Morty-Font/get_schwifty.ttf" */
-
-function Titulo() {
-  return <h1 className={style.titulo}>Rick and Morty</h1>;
-}
-
-export default Titulo;
