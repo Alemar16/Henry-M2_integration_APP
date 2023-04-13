d9abdf5a1aa28551ac5f6f659556d43e69d0f36d
Armando Martinez <alemar.martinez16@gmail.com>
2023-04-13 02:14:42 -0300

    Esrilos en Favoritos.jsx

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -29,46 +29,57 @@ const Favorites = () => {
   
   return (
     <div className={styles.background}>
-      <h1 className={styles.titulo}>Mis Favoritos</h1>
+      <div className={styles.containerFav}>
+        <div className={styles.leftContainer}>
+          <h1 className={styles.tituloFavoritos}>Mis Favoritos</h1>
 
-      <div className={styles.selectores}>
-        <select id="orderSelect" onChange={handleOrder}>
-          <option value="order" disabled>
-            Ordenar en
-          </option>
-          <option value="Ascendente">Ascendente</option>
-          <option value="Decendente">Descendente</option>
-        </select>
+          <div className={styles.selectores}>
+            <select
+              id="orderSelect"
+              onChange={handleOrder}
+              defaultValue="order"
+            >
+              <option value="order" disabled>
+                Ordenar en
+              </option>
+              <option value="Ascendente">Ascendente</option>
+              <option value="Decendente">Descendente</option>
+            </select>
 
-        <select
-          id="filterSelect"
-          onChange={(event) => {
-            if (event.target.value === "reset") {
-              handleReset();
-            } else {
-              handleFilter(event);
-            }
-          }}
-        >
-          <option value="filter" disabled>
-            Filtrar por
-          </option>
-          <option value="Male">Masculino</option>
-          <option value="Female">Femenino</option>
-          <option value="Genderless">Sin Genero</option>
-          <option value="unknown">Desconocido</option>
-          <option value="reset">Restablecer</option>
-        </select>
+            <select
+              id="filterSelect"
+              onChange={(event) => {
+                if (event.target.value === "reset") {
+                  handleReset();
+                } else {
+                  handleFilter(event);
+                }
+              }}
+              defaultValue="filter"
+            >
+              <option value="filter" disabled>
+                Filtrar por
+              </option>
+              <option value="Male">Masculino</option>
+              <option value="Female">Femenino</option>
+              <option value="Genderless">Sin Genero</option>
+              <option value="unknown">Desconocido</option>
+              <option value="reset">Restablecer</option>
+            </select>
 
-    {/*  <button value="reset" onClick={handleReset}>
+            {/*  <button value="reset" onClick={handleReset}>
           Restablecer
         </button> */}
-      </div>
+          </div>
+        </div>
 
-      <div className={styles.container}>
-        {favorites.map((fav) => (
-          <Card key={fav.id} {...fav} isFavorite={true} />
-        ))}
+        <div className={styles.rightContainer}>
+          <div className={styles.containerFavoritos}>
+            {favorites.map((fav) => (
+              <Card key={fav.id} {...fav} isFavorite={true} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
