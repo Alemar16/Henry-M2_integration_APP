3b143682dcfdd4f9ea20d2f7c83acfc9e62c6882
Armando Martinez <alemar.martinez16@gmail.com>
2023-04-20 21:55:25 -0300

    Async/Await en addFavorite y removeFavorite

--- Client/src/components/Redux/actions.js
+++ Client/src/components/Redux/actions.js
@@ -3,13 +3,38 @@ import { ADD_FAVORITE, REMOVE_FAVORITE, FILTER, ORDER } from "./action.types";
 const endpoint = "http://localhost:3001/rickandmorty/fav";
 
 // -----actions creators -------------------------
-//metodo anterior sin Express
-/* export const addFavorite = (character) => {
-  return {type: ADD_FAVORITE, payload: character }
-} */
 
-// CON EXPRESS
-export const addFavorite = (character) => {
+//=============METODO CON ASYNC AWAIT===================================
+  export const addFavorite = (character) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post(endpoint, character);
+      return dispatch({
+        type: ADD_FAVORITE,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const removeFavorite = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.delete(`${endpoint}/${id}`);
+      return dispatch({
+        type: REMOVE_FAVORITE,
+        payload: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};  
+
+//================CON EXPRESS Y PROMESAS================================
+/* export const addFavorite = (character) => {
   return (dispatch) => {
     axios.post(endpoint, character).then(({ data }) => {
       return dispatch({
@@ -20,11 +45,6 @@ export const addFavorite = (character) => {
   };
 };
 
-//Metodo anterior sin Express
-/* export const removeFavorite = (id) => {
-  return{ type: REMOVE_FAVORITE, payload: id}
-} */
-
 export const removeFavorite = (id) => {
   return (dispatch) => {
     axios.delete(`${endpoint}/${id}`).then(({ data }) => {
@@ -34,7 +54,20 @@ export const removeFavorite = (id) => {
       });
     });
   };
-};
+};  */ 
+
+//===========METODO ANTERIOR SIN EXPRESS=================
+
+/* export const addFavorite = (character) => {
+  return {type: ADD_FAVORITE, payload: character }
+} */
+
+
+/* export const removeFavorite = (id) => {
+  return{ type: REMOVE_FAVORITE, payload: id}
+} */
+//=======================================================
+
 
 export const filterCards = (gender) => {
   return { type: FILTER, payload: gender };
